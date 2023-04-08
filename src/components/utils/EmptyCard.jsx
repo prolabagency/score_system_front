d8652cf4ae0b13632d166f9f8ab73be36ff3465d
Orozbek <oroz.jenishbek.2003@gmail.com>
2023-04-08 15:38:19 +0600

    ...

--- src/components/utils/EmptyCard.jsx
+++ src/components/utils/EmptyCard.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const EmptyCard = ({manager, className = '', ...props}) => {
-    if (!manager.isFetching && manager.data.results.length === 0) {
+    if (!manager.isFetching && manager.data.results?.length || null === 0) {
         return (
             <div className={`card text-center ${className}`} {...props}>
                 Ничего не найдено или не существует данных
